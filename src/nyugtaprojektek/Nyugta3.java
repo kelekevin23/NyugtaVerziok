b3d992b1d6509efefcaefd644bc7f17a179f7456
kelekevin23 <kelekevin11@gmail.com>
2022-03-23 11:17:53 +0100

    nyugta3 kód rendszerezése

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -8,43 +8,42 @@ a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzegh
 public class Nyugta3 {
 
     public static void main(String[] args) {
+        
+        final String HUF = "Ft";
+        final String eur = "\u20ac";
+        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
+        int osszesen = tetel1 + tetel2 + tetel3;
+        int szervizDijMertek = 10;
+        
         String csillagok = "********************";
+        String duplaVonal = "====================";
+        String szaggatottVonal = "--------------------";
+        String rovidVonal = "_______";
+        
         System.out.println(csillagok);
         System.out.printf("%14s\n", "Nyugta 3");
         System.out.println(csillagok);
 
-        final String HUF = "Ft";
-        int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
         System.out.printf("%10s:%6d%3s\n", "Tétel 1", tetel1, HUF);
         System.out.printf("%10s:%6d%3s\n", "Tétel 2", tetel2, HUF);
         System.out.printf("%10s:%6d%3s\n", "Tétel 3", tetel3, HUF);
 
-        String duplaVonal = "====================";
         System.out.println(duplaVonal);
-
-        int osszesen = tetel1 + tetel2 + tetel3;
         System.out.printf("%10s:%6d%3s\n", "Összesen", osszesen, HUF);
-
-        String szaggatottVonal = "--------------------";
         System.out.println(szaggatottVonal);
 
-        int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
         System.out.printf("%10s:%6d%3s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
-
         System.out.println(duplaVonal);
 
         int fizetendo = osszesen + szervizDij;
         System.out.printf("%10s:%6d%3s\n", "Fizetendő", fizetendo, HUF);
+        
         double euro = fizetendo / 350.0;
-
-        final String eur = "\u20ac";
         System.out.printf("%10s%7.2f%2s\n", "", euro, eur);//
-
         System.out.println(szaggatottVonal + "\n");
 
-        String rovidVonal = "_______";
         System.out.print(rovidVonal);
         System.out.printf("%13s\n", rovidVonal);
 
